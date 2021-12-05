73a15ff997e3b85694fc6c48e848c0caa96e49dd
Frogoman <carlossb23@gmail.com>
2021-12-05 18:22:48 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,15 +2,15 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("logo").style.width = "40px";
-    document.getElementById("logo").style.margin = "5px";
-    document.getElementById("logo_li").style.width = "50px";
-  } else {
-    document.getElementById("logo").style.width = "100px";
-    document.getElementById("logo").style.margin = "10px";
-    document.getElementById("logo_li").style.width = "120px";
-  }
+  	if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    	document.getElementById("logo").style.width = "40px";
+    	document.getElementById("logo").style.margin = "5px";
+    	document.getElementById("logo_li").style.width = "50px";
+  	} else {
+		document.getElementById("logo").style.width = "100px";
+	  	document.getElementById("logo").style.margin = "10px";
+		document.getElementById("logo_li").style.width = "120px";
+  	}
 }
 
 
